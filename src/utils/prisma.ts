727d554e683a4ac71927a0f756afc375136d5b6d
Olesia Shpikovskaia <oshpikovskaia@gmail.com>
2025-12-02 11:36:53 +0200

    chore(prisma): switch to direct Postgres URL without Accelerate

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,18 +1,10 @@
-import { withAccelerate } from '@prisma/extension-accelerate';
-
 import { PrismaClient } from '@/generated/prisma/client';
 
-const createPrismaClient = () => {
-  return new PrismaClient().$extends(withAccelerate());
-};
-
-type ExtendedPrismaClient = ReturnType<typeof createPrismaClient>;
-
 const globalForPrisma = globalThis as unknown as {
-  prisma?: ExtendedPrismaClient;
+  prisma?: PrismaClient;
 };
 
-const prisma: ExtendedPrismaClient = globalForPrisma.prisma ?? createPrismaClient();
+const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') {
   globalForPrisma.prisma = prisma;
