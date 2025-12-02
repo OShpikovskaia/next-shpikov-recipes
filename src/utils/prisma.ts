b94d1e2e5abbaadbe7d12793bfcb71390a6ec8ce
Olesia Shpikovskaia <oshpikovskaia@gmail.com>
2025-12-02 11:13:48 +0200

    update prisma.ts

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -2,12 +2,20 @@ import { withAccelerate } from '@prisma/extension-accelerate';
 
 import { PrismaClient } from '@/generated/prisma/client';
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient;
+const createPrismaClient = () => {
+  return new PrismaClient().$extends(withAccelerate());
 };
 
-const prisma = globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
+type ExtendedPrismaClient = ReturnType<typeof createPrismaClient>;
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+const globalForPrisma = globalThis as unknown as {
+  prisma?: ExtendedPrismaClient;
+};
+
+const prisma: ExtendedPrismaClient = globalForPrisma.prisma ?? createPrismaClient();
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma;
+}
 
 export default prisma;
